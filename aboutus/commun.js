b1c0d72ae06619bf8ebe752d3e8831041685ad82
mrzakx85 <mrzakxini@gmail.com>
2025-12-04 11:18:50 -0800

    Add files via upload

---
+++ aboutus/commun.js
@@ -0,0 +1,15 @@
+document.getElementById("mainPage").addEventListener("click", () => {
+  window.location.href = "../index.html";
+});
+
+document.getElementById("educationSystem").addEventListener("click", () => {
+  window.location.href = "../educationsystem/index.html";
+});
+
+document.getElementById("aboutUs").addEventListener("click", () => {
+  window.location.href = "../aboutus/index.html";
+});
+
+document.getElementById("contact").addEventListener("click", () => {
+  window.location.href = "../contactus/index.html";
+});
