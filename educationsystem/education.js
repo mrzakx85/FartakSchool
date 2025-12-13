0e9806f739cb9050aaca267852a39ecf4d8c8b7d
mrzakx85 <mrzakxini@gmail.com>
2025-12-13 12:36:15 -0800

    Add files via upload

---
+++ educationsystem/education.js
@@ -0,0 +1,16 @@
+document.getElementById("mainPage").addEventListener("click", () => {
+  window.location.href = "../index.html";
+});
+
+document.getElementById("educationSystem").addEventListener("click", () => {
+  window.location.href = "./index.html";
+});
+
+
+document.getElementById("aboutUs").addEventListener("click", () => {
+  window.location.href = "../aboutus/index.html";
+});
+
+document.getElementById("contact").addEventListener("click", () => {
+  window.location.href = "../contactus/index.html";
+});
