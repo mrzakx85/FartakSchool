6fef5f5fe6d1509a6afef676aa94686758170e3e
mrzakx85 <mrzakxini@gmail.com>
2026-01-30 06:09:03 -0800

    Update commun.js

--- aboutus/commun.js
+++ aboutus/commun.js
@@ -1,36 +1,87 @@
-document.getElementById("mainPage").addEventListener("click", () => {
+
+const f1 = document.getElementById("f1");
+const f2 = document.getElementById("f2");
+const f3 = document.getElementById("f3");
+
+const f1Target = document.getElementById("f1-target");
+const f2Target = document.getElementById("f2-target");
+const f3Target = document.getElementById("f3-target");
+
+
+f1.addEventListener("click", function () {
+  f1Target.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+  });
+});
+
+f2.addEventListener("click", function () {
+  f2Target.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+  });
+});
+
+f3.addEventListener("click", function () {
+  f3Target.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+  });
+});
+
+
+const infoModalBtn = document.getElementById("infoModalBtn");
+const mainPage = document.getElementById("mainPage");
+const educationSystem = document.getElementById("educationSystem");
+const aboutUs = document.getElementById("aboutUs");
+const contact = document.getElementById("contact");
+const menu = document.getElementById("openSlideNav");
+const slideNavBox = document.querySelector(".slidablenav-box");
+
+
+
+
+
+mainPage.addEventListener("click", () => {
   window.location.href = "../index.html";
 });
 
-document.getElementById("educationSystem").addEventListener("click", () => {
+educationSystem.addEventListener("click", () => {
   window.location.href = "../educationsystem/index.html";
 });
 
-document.getElementById("aboutUs").addEventListener("click", () => {
-  window.location.href = "../aboutus/index.html";
+aboutUs.addEventListener("click", () => {
+  window.location.href = "./index.html";
 });
 
-document.getElementById("contact").addEventListener("click", () => {
+contact.addEventListener("click", () => {
   window.location.href = "../contactus/index.html";
 });
 
-document.getElementById("f1").addEventListener("click", function () {
-  document.getElementById("f1-target").scrollIntoView({
-    behavior: "smooth",
-    block: "center",
-  });
+
+const SlideNavMainPage = document.querySelector(".slidablenav #mainPage");
+const SlideNavEducationSystem = document.querySelector(".slidablenav #educationSystem");
+const SlideNavAboutUs = document.querySelector(".slidablenav #aboutUs");
+const SlideNavContact = document.querySelector(".slidablenav #contact");
+
+
+SlideNavMainPage.addEventListener("click", () => {
+  window.location.href = "../index.html";
 });
 
-document.getElementById("f2").addEventListener("click", function () {
-  document.getElementById("f2-target").scrollIntoView({
-    behavior: "smooth",
-    block: "center",
-  });
+SlideNavEducationSystem.addEventListener("click", () => {
+  window.location.href = "../educationsystem/index.html";
 });
 
-document.getElementById("f3").addEventListener("click", function () {
-  document.getElementById("f3-target").scrollIntoView({
-    behavior: "smooth",
-    block: "center",
-  });
+SlideNavAboutUs.addEventListener("click", () => {
+  window.location.href = "./index.html";
 });
+
+SlideNavContact.addEventListener("click", () => {
+  window.location.href = "../contactus/index.html";
+});
+
+function openSlidableNav() {
+  slideNavBox.classList.toggle("open");
+}
+menu.addEventListener("click" , openSlidableNav);
