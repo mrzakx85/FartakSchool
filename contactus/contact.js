71e3d002b4054bb718012093a43781e469ddf1cc
mrzakx85 <mrzakxini@gmail.com>
2026-01-30 06:04:50 -0800

    Update contact.js

--- contactus/contact.js
+++ contactus/contact.js
@@ -1,15 +1,72 @@
-document.getElementById("mainPage").addEventListener("click", () => {
+// document.getElementById("mainPage").addEventListener("click", () => {
+//   window.location.href = "../index.html";
+// });
+
+// document.getElementById("educationSystem").addEventListener("click", () => {
+//   window.location.href = "../educationsystem/index.html";
+// });
+
+// document.getElementById("aboutUs").addEventListener("click", () => {
+//   window.location.href = "../aboutus/index.html";
+// });
+
+// document.getElementById("contact").addEventListener("click", () => {
+//   window.location.href = "../contactus/index.html";
+// });
+
+
+const infoModalBtn = document.getElementById("infoModalBtn");
+const mainPage = document.getElementById("mainPage");
+const educationSystem = document.getElementById("educationSystem");
+const aboutUs = document.getElementById("aboutUs");
+const contact = document.getElementById("contact");
+const menu = document.getElementById("openSlideNav");
+const slideNavBox = document.querySelector(".slidablenav-box");
+
+
+
+
+
+mainPage.addEventListener("click", () => {
   window.location.href = "../index.html";
 });
 
-document.getElementById("educationSystem").addEventListener("click", () => {
+educationSystem.addEventListener("click", () => {
   window.location.href = "../educationsystem/index.html";
 });
 
-document.getElementById("aboutUs").addEventListener("click", () => {
+aboutUs.addEventListener("click", () => {
   window.location.href = "../aboutus/index.html";
 });
 
-document.getElementById("contact").addEventListener("click", () => {
-  window.location.href = "../contactus/index.html";
+contact.addEventListener("click", () => {
+  window.location.href = "./index.html";
 });
+
+
+const SlideNavMainPage = document.querySelector(".slidablenav #mainPage");
+const SlideNavEducationSystem = document.querySelector(".slidablenav #educationSystem");
+const SlideNavAboutUs = document.querySelector(".slidablenav #aboutUs");
+const SlideNavContact = document.querySelector(".slidablenav #contact");
+
+
+SlideNavMainPage.addEventListener("click", () => {
+  window.location.href = "../index.html";
+});
+
+SlideNavEducationSystem.addEventListener("click", () => {
+  window.location.href = "../educationsystem/index.html";
+});
+
+SlideNavAboutUs.addEventListener("click", () => {
+  window.location.href = "../aboutus//index.html";
+});
+
+SlideNavContact.addEventListener("click", () => {
+  window.location.href = "./index.html";
+});
+
+function openSlidableNav() {
+  slideNavBox.classList.toggle("open");
+}
+menu.addEventListener("click" , openSlidableNav);
