12c7972d5a88c273f3ac102ed20bc7486713bcc5
mrzakx85 <mrzakxini@gmail.com>
2026-06-14 02:35:37 -0700

    Add files via upload

---
+++ gallery/main.js
@@ -0,0 +1,32 @@
+const navPreregister = document.getElementById("navPreregister");
+const facility = document.getElementById("facility");
+const loginBtn = document.querySelector(".login-btn");
+const logo = document.querySelector(".logo-box");
+const plans = document.getElementById("plans");
+const contact = document.getElementById("contact");
+
+navPreregister.addEventListener("click", () => {
+  window.location.href = "../preRegister/index.html";
+});
+
+facility.addEventListener("click", () => {
+  window.location.href = "../facilities/index.html";
+});
+
+loginBtn.addEventListener("click", () => {
+  window.location.href = "../logIn/index.html";
+});
+
+logo.addEventListener("click", () => {
+  window.location.href = "../index.html";
+});
+
+plans.addEventListener("click", () => {
+  window.location.href = "../plans/index.html";
+});
+
+contact.addEventListener("click", () => {
+  window.location.href = "../contactUs/index.html";
+});
+
+// // //  //  //
